da4c9a8d9ef4a87a4a4918c6084ee1a44983f547
OnelieKata <jordanboucher@voila.fr>
2015-05-18 16:56:33 +0200

    fef

---
+++ projet-Fanorona-L3-2015/src/IHM/EcouteurDeBouton.java
@@ -0,0 +1,19 @@
+package IHM;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+
+import engine.*;
+import AI.*;
+
+public class EcouteurDeBouton implements ActionListener{
+	
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
---
+++ projet-Fanorona-L3-2015/src/IHM/EcouteurDeSouris.java
@@ -0,0 +1,29 @@
+package IHM;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class EcouteurDeSouris implements MouseListener{
+	AireDeDessin aire;
+	
+	public EcouteurDeSouris(AireDeDessin a){
+		aire=a;
+	}
+	
+	
+	
+	public void mouseClicked(MouseEvent e) {
+	}
+	public void mouseEntered(MouseEvent e) {
+	}
+	public void mouseExited(MouseEvent e) {
+	}
+	public void mousePressed(MouseEvent e) {
+	}
+	public void mouseReleased(MouseEvent e) {
+	}
+
+}
