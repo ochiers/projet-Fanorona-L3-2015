a9cdb1994cd358034ae46bd0819bf63e06c7f128
OnelieKata <jordanboucher@voila.fr>
2015-05-18 11:41:44 +0200

    IHM grille

--- projet-Fanorona-L3-2015/src/IHM/AireDeDessin.java
+++ projet-Fanorona-L3-2015/src/IHM/AireDeDessin.java
@@ -37,14 +37,25 @@ class AireDeDessin extends JComponent {
     	for(int i=0;i<=8;i++){
     		drawable.drawLine(10+i*taille, 10, 10+i*taille,10+4*taille);
     	}
+    	
     		//ligne Horizontale
     	for(int i=0;i<=4;i++){
     		drawable.drawLine(10, 10+i*taille, 10+8*taille, 10+i*taille);
     	}
+    	
     		//diagonale decroissante
+    	drawable.drawLine(10,10+2*taille,10+2*taille,10+4*taille);
+    	for(int i=0;i<3;i++){
+    		drawable.drawLine(10+(2*i*taille),10,10+(2*i*taille)+4*taille,10+4*taille);
+    	}
+    	drawable.drawLine(10+6*taille,10,10+8*taille,10+2*taille);
     	
     		//diagonale croissante
-    	System.out.println("test");
+    	/*drawable.drawLine(10,10+2*taille,10+2*taille,10+4*taille);
+    	for(int i=0;i<3;i++){
+    		drawable.drawLine(10+(2*i*taille),10,10+(2*i*taille)+4*taille,10+4*taille);
+    	}
+    	drawable.drawLine(10+6*taille,10,10+8*taille,10+2*taille);*/
     
     }
     
