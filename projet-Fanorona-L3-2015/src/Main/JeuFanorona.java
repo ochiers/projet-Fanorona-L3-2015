ed04fe333bf8c79abfdd86017652896bcb974448
fernajer <fernajer@127.0.0.1>
2015-05-26 14:02:16 +0200

    Création IA facile

--- projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
+++ projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.ArrayList;
+
 import javax.swing.SwingUtilities;
 
 import engine.*;
@@ -11,12 +13,21 @@ public class JeuFanorona {
 		Engine e=new Engine();
 		
 		Player p1=new HumanPlayer(e,false,"player1");
-		Player p2=new HumanPlayer(e,false,"player2");
+		//Player p1=new EasyAI(e,true,"player1");
+		Player p2=new EasyAI(e,true,"player2");
 
 		Fenetre f=new Fenetre(e);
 		e.setAffichage(f);
 		
 		e.nouvellePartie(p1, p2,0, 5,9);
+		// ajouté pour test ia
+//		ArrayList<Case> pionsPossibles = e.partieCourante.lesPionsQuiPeuventManger();
+//		Case[] tmp = new Case[pionsPossibles.size()];
+//		Coup c = e.partieCourante.joueurCourant.play(pionsPossibles.toArray(tmp));
+//		System.out.println("--------------");
+//		System.out.println("Depart (" + c.depart.ligne + "," + c.depart.colonne + ") Arrivée (" + c.arrivee.ligne + ","  + c.arrivee.colonne + ")");
+//		System.out.println("--------------");
+		//fin ajout
 		SwingUtilities.invokeLater(f);
 		Thread.sleep(200);
 		e.begin();
