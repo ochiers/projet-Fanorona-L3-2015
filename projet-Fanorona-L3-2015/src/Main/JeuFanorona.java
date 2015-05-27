fc981c5836f3ee81ac580167a95348d0124c7ea3
fernajer <fernajer@127.0.0.1>
2015-05-27 10:11:01 +0200

    ddfddf

--- projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
+++ projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
@@ -12,9 +12,9 @@ public class JeuFanorona {
 	public static void main(String argv[]) throws InterruptedException{
 		Engine e=new Engine();
 		
-		Player p1=new HumanPlayer(e,false,"player1");
+		Player p1=new HumanPlayer(e,false,"Jeremy");
 		//Player p1=new EasyAI(e,true,"player1");
-		Player p2=new EasyAI(e,true,"player2");
+		Player p2=new HardAI(e,true,"player2");
 
 		Fenetre f=new Fenetre(e);
 		e.setAffichage(f);
