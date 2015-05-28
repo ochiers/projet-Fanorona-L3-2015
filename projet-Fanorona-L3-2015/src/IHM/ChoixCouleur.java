86a1cfeced7acc193e91a4044ca2920423e64f46
msi justine <msi justine@130.190.30.137>
2015-05-28 16:20:09 +0200

    IHM choix couleur pions en cours - compilation OK

--- projet-Fanorona-L3-2015/src/IHM/ChoixCouleur.java
+++ projet-Fanorona-L3-2015/src/IHM/ChoixCouleur.java
@@ -1,35 +1,3 @@
-/*
-	 * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
-	 *
-	 * Redistribution and use in source and binary forms, with or without
-	 * modification, are permitted provided that the following conditions
-	 * are met:
-	 *
-	 *   - Redistributions of source code must retain the above copyright
-	 *     notice, this list of conditions and the following disclaimer.
-	 *
-	 *   - Redistributions in binary form must reproduce the above copyright
-	 *     notice, this list of conditions and the following disclaimer in the
-	 *     documentation and/or other materials provided with the distribution.
-	 *
-	 *   - Neither the name of Oracle or the names of its
-	 *     contributors may be used to endorse or promote products derived
-	 *     from this software without specific prior written permission.
-	 *
-	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-	 * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-	 * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-	 * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-	 * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	 * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	 * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	 * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	 * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	 * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	 * 
-	 * Base recuperee pour ce code.
-*/ 
 package IHM;
 
 import java.awt.*;
@@ -40,7 +8,7 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class ChoixCouleur extends JPanel {
 
-		ActionListener okListener, cancelListener;
+		ActionListener okListener, cancelListener, resetListener;
 	
 	    protected JColorChooser fenChoix1;
 	    protected JColorChooser fenChoix2;
@@ -58,25 +26,36 @@ public class ChoixCouleur extends JPanel {
 	        titre.setFont(new Font("SansSerif", Font.BOLD, 24));
 	        titre.setPreferredSize(new Dimension(100, 65));
 	        	//creation panel conteneur
-	        JPanel panelTitre = new JPanel(new BorderLayout());
-	        //JButton bcc = new JButton(" VALIDER ");
-	        //	bcc.addActionListener(this);	        
+	        JPanel panelTitre = new JPanel(new BorderLayout());	        
 	        panelTitre.add(titre, BorderLayout.CENTER);
-	        //panelTitre.add(bcc);
-	        panelTitre.setBorder(BorderFactory.createTitledBorder("titre"));
+	        	//creation panel central
+	        JPanel panelCentre = new JPanel(new GridLayout(3,1));
 	        	//mettre le colorchooser en place pour joueur 1
-	        fenChoix1 = new JColorChooser(titre.getForeground());
+	        fenChoix1 = new JColorChooser(Color.black);
 	        fenChoix1.setBorder(BorderFactory.createTitledBorder(" Pions Joueur 1 "));
 	        fenChoix1.setPreviewPanel(new JPanel());
+	        panelCentre.add(fenChoix1);
 	        	//mettre le colorchooser en place pour joueur 2
-	        fenChoix2 = new JColorChooser(titre.getForeground());
-	        fenChoix2.createDialog((Component)fenChoix2, titre.getText(), true, fenChoix2, okListener, cancelListener);
+	        fenChoix2 = new JColorChooser(Color.white);
 	        fenChoix2.setBorder(BorderFactory.createTitledBorder(" Pions Joueur 2 "));
 	        fenChoix2.setPreviewPanel(new JPanel());
+	        panelCentre.add(fenChoix2);
+	        	//creation panel boutons
+	        JPanel panelBouton = new JPanel(new GridLayout(1,3));
+	        	//mettre 3 boutons
+		    JButton ok = new JButton(" VALIDER ");
+	        	ok.addActionListener(new okListener());
+	        JButton annuler = new JButton(" ANNULER ");
+	        	annuler.addActionListener(new cancelListener());
+		    JButton reset = new JButton(" REMETTRE PAR DEFAUT ");
+	        	reset.addActionListener(new resetListener());
+	        panelBouton.add(ok);
+	        panelBouton.add(annuler);
+	        panelBouton.add(reset);
 	        	// ajouts
 	        add(panelTitre, BorderLayout.NORTH);
-	        add(fenChoix1, BorderLayout.CENTER);
-	        add(fenChoix2, BorderLayout.PAGE_END);
+	        add(panelCentre, BorderLayout.CENTER);
+	        add(panelBouton, BorderLayout.SOUTH);
 	    }
 
 	    private static void createAndShow() {
