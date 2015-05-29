6b9d7cc523d9fba2cd01de99af9ca89bebc83d28
ochiers <sebastien.ochier@gmail.com>
2015-05-29 10:32:07 +0200

    Pose des bases pour le réseau.

--- projet-Fanorona-L3-2015/src/network/PlayerOnNetwork.java
+++ projet-Fanorona-L3-2015/src/network/PlayerOnNetwork.java
@@ -2,4 +2,56 @@ package network;
 
 public class PlayerOnNetwork {
 
+	// TODO 1. Etablir la connexion
+	// TODO 2. Envoyer la configuration
+	// TODO 3. Envoyer un coup valide au moteur distant
+	// TODO 4. Envoyer le coup au joueur (moteur -> joueur)
+	
+	//TODO Système de requêtes : Envoie d'une annulation/refaire
+
+	
+	// Comment déterminer celui qui commence ?
+	// Le moteur analyse en local les coups tenter par le joueur humain puis l'envoie sur le réseau. Seul des coups valides sont envoyés.
+	// Comment annuler et refaire en réseau ? Requête ? 
+	
+	/**
+	 * Etablie la connexion entre les deux ordinateurs.
+	 * Elle prend une adresse ip ainsi qu'un numéro de port pour l'établir.
+	 */
+	public void etablishConnection()
+	{ // Une ip et un port
+
+	}
+
+	/**
+	 * Envoie de la configuration de la machine principale vers la deuxième.
+	 */
+	public void sendConfig()
+	{
+
+	}
+
+	/**
+	 * Méthode permettant de recevoir la configuration partagée entre les 2 ordinateurs.
+	 */
+	public void receiveConfig()
+	{
+
+	}
+
+	/**
+	 * Envoie du coup valide du joueur humain.
+	 */
+	public void sendCoup()
+	{
+
+	}
+
+	/**
+	 * Réception du coup envoyé sur le réseau.
+	 */
+	public void receiveCoup()
+	{
+
+	}
 }
