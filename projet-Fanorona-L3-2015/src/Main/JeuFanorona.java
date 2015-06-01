86b86ab9d78b0bb8432eec34cb68d52e342d9989
ochiers <sebastien.ochier@gmail.com>
2015-06-01 14:29:22 +0200

    Commit

--- projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
+++ projet-Fanorona-L3-2015/src/Main/JeuFanorona.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 
 import javax.swing.SwingUtilities;
 
+import network.NetworkPlayer;
 import engine.*;
 import IHM.*;
 import AI.*;
@@ -12,13 +13,14 @@ public class JeuFanorona {
 	public static void main(String argv[]) throws InterruptedException{
 
 		EngineServices e=new Engine();
-		Player p1=new HumanPlayer(e,false,"cLEM");
+		Player p1=new HumanPlayer(e,false,"seb");
 		//Player p1=new EasyAI(e,true,"player1");
-		Player p2=new EasyAI(e,true,"player2");
+		Player p2=new NetworkPlayer(e,false,"clem");
 		Fenetre f= new Fenetre(e);
 		e.setDisplay(f);
+		e.rejoindrePartie(12345, "152.77.82.223");
 		// System.err.close();
-		e.nouvellePartie(p1, p2,0, new Dimension(9,5));
+		e.nouvellePartie(p2, p1,0, new Dimension(9,5));
 		SwingUtilities.invokeLater(f);
 		Thread.sleep(200);
 		e.begin();
