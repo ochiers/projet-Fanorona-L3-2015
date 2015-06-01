e407b45ce6ad3b9072396613980b95d4c4e2d9ae
msi justine <msi justine@130.190.29.230>
2015-06-01 11:49:22 +0200

    Revert "version a bug pour preferences"
    This reverts commit dc915983edb4bd400b2188ea2b23dd570c081169.

--- projet-Fanorona-L3-2015/src/IHM/ChoixNoms.java
+++ projet-Fanorona-L3-2015/src/IHM/ChoixNoms.java
@@ -13,8 +13,6 @@ import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class ChoixNoms extends JPanel {
-	static int pw = 699;
-	static int ph = 549;
 	
 	public ChoixNoms() {
 
@@ -38,18 +36,19 @@ public class ChoixNoms extends JPanel {
 		nom.setBounds(width, 50, width, height);
 		name1.setBounds(width, 150, width, height);
 		name2.setBounds(width, 350, width, height);
-			// ajout fond
-	    ImagePanel imagefond = new ImagePanel(new ImageIcon("src/images/players.jpg").getImage(), pw, ph);
 			//ajouts
 		add(nom);
 		add(name1);
 		add(name2);
-		add(imagefond);
 	}
 	
-    private static void createAndShow() {		
+    private static void createAndShow() {
+		int pw = 699;
+		int ph = 549;
+		
     		//fenetre
 	    JFrame frame = new JFrame("Choix noms joueurs");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    	//ajout case texte
 	    JPanel tmpPanel = new ChoixNoms();
 	    frame.setContentPane(tmpPanel);
@@ -57,9 +56,9 @@ public class ChoixNoms extends JPanel {
 	    frame.setVisible(true);
 	}
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 	    javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	        public void run() { createAndShow(); }
 	    });
-	}
+	}*/
 }
