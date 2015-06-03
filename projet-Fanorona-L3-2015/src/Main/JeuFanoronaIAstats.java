b4d75c81e61bd0471509206a8fe2e4585d2dfe19
fernajer <fernajer@127.0.0.1>
2015-06-03 16:30:26 +0200

    Modifs executable stats + màj journal

--- projet-Fanorona-L3-2015/src/Main/JeuFanoronaIAstats.java
+++ projet-Fanorona-L3-2015/src/Main/JeuFanoronaIAstats.java
@@ -6,31 +6,41 @@ import engine.*;
 import AI.*;
 
 public class JeuFanoronaIAstats {
-	public static void main(String argv[]) throws InterruptedException{
+	public static void main(String argv[]) throws InterruptedException {
 
-		EngineServices e=new Engine();
-		Player p1 = new EasyAI(e,true,"Chuck Norris");
-		Player p2 = new EasyAI(e,true,"Guillaume de Sauza");
+		Machin[] listeParties = new Machin[100];
+		for (int i = 0; i < 2; i++) {
+			Machin m = new Machin();
+			listeParties[i] = m;
+			m.start();
+		}
+		for (int i = 0; i < 2; i++) {
+			listeParties[i].join();
+		}
+		Thread.sleep(5000);
+		System.out.flush();
+		System.out.println("Nb victoires J1 : " + Machin.nbWInsJoueur1);
+		System.out.println("Nb victoires J2 : " + Machin.nbWInsJoueur2);
+	}
+}
+
+class Machin extends Thread {
+	public static int nbWInsJoueur1;
+	public static int nbWInsJoueur2;
+
+	public void run() {
+		EngineServices e = new Engine();
+		Player p1 = new EasyAI(e, true, "Chuck Norris");
+		Player p2 = new EasyAI(e, true, "Guillaume de Sauza");
 		AffichageVide f = new AffichageVide();
 		e.setDisplay(f);
 		System.err.close();
-		int nbWInsJoueur1 = 0;
-		int nbWInsJoueur2 = 0;
-		for(int i = 0; i < 100; i++){
-			System.out.println("test");
-			if(i%2 == 0)
-				e.nouvellePartie(p1, p2,0, new Dimension(9,5));
-			else e.nouvellePartie(p2, p1,0, new Dimension(9,5));
-			if(i == 0){
-				e.begin();
-			}
-			Player vainqueur = e.getWinner();
-			System.out.println(vainqueur);
-			if(vainqueur == p1)
-				nbWInsJoueur1++;
-			else nbWInsJoueur2++;
-		}
-		System.out.println("Nb victoires J1 : " + nbWInsJoueur1);
-		System.out.println("Nb victoires J2 : " + nbWInsJoueur2);
+		e.nouvellePartie(p1, p2, 0, new Dimension(9, 5));
+		e.begin();
+		Player vainqueur = e.getWinner();
+		if (vainqueur == p1)
+			nbWInsJoueur1++;
+		else
+			nbWInsJoueur2++;
 	}
 }
