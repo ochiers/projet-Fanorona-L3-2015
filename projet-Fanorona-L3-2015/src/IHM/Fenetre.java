b97bdd7b7ca1f14820a229c9782447270281e0f6
ochiers <sebastien.ochier@gmail.com>
2015-06-08 16:30:23 +0200

    Update fond à l'ouverture

--- projet-Fanorona-L3-2015/src/IHM/Fenetre.java
+++ projet-Fanorona-L3-2015/src/IHM/Fenetre.java
@@ -255,17 +255,15 @@ public class Fenetre implements Runnable, Affichage {
 		preference = new PreferencesOnglets(this);
 		preference.majPref();
 
-		//maj
-		fw = frame.getWidth();
-		fh = frame.getHeight();
-		frame.repaint();
-		
 		// FENETRE
 		frame.setJMenuBar(menuBar);
 		frame.add(getPanelAccueil());
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.addWindowListener(new EcouteurDeFenetre(engine));
+		fw = frame.getWidth();
+		fh = frame.getHeight();
+		frame.repaint();
 	}
 
 	// ACTIONLISTENER
