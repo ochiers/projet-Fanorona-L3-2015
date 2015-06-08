388db055c3701ee0c457498780e7a9c5c7a0f54e
ochiers <sebastien.ochier@gmail.com>
2015-06-08 11:18:12 +0200

    Changement couleur des pions effectue Bug au changement de couleur, quelques pions peuvent rester inchangés

--- projet-Fanorona-L3-2015/src/IHM/PionButton.java
+++ projet-Fanorona-L3-2015/src/IHM/PionButton.java
@@ -6,15 +6,15 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class PionButton extends JButton{
+public class PionButton extends JButton {
 
 	private static final long	serialVersionUID	= 7393526125348622611L;
-	public Fenetre frame;
-	public JButton button;
-	public String fichierPion;
-	
-	
-	public PionButton(String fichier){
+	public Fenetre				frame;
+	public JButton				button;
+	public String				fichierPion;
+
+	public PionButton(String fichier)
+	{
 		this.button = new JButton(new ImageIcon(fichier));
 		this.fichierPion = fichier;
 	}
@@ -26,8 +26,7 @@ class selectionPion implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		
-		
+
 	}
-	
+
 }
